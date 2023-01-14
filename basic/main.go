9904354e84ba1aeb21ccf7ebd90c95fac6665f02
Brian Hartford <bhartford419@gmail.com>
2023-01-14 03:24:02 -0500

    Update main.go

--- basic/main.go
+++ basic/main.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	"github.com/UTXOnly/relayer"
-	"github.com/fiatjaf/UTXOnly/storage/postgresql"
+	"github.com/UTXOnly/storage/postgresql"
 	"github.com/kelseyhightower/envconfig"
 	"github.com/nbd-wtf/go-nostr"
 )
